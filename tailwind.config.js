498c46b14b3cfc37315e94d979bee44419f5f2a0
Silviu Miron <silviu_miron_90@outlook.com>
2024-03-17 21:20:28 +0200

    Fix : Fix first-contatiner layout and info-container.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./docs/**/*.{html,js}"],
+  content: ['./docs/**/*.{html,js}'],
   theme: {
     borderRadius: {
-      none: "0",
-      sm: "0.125rem",
-      DEFAULT: "0.25rem",
-      DEFAULT: "4px",
-      md: "0.375rem",
-      lg: "0.5rem",
-      full: "99px",
-      large: "12px",
+      none: '0',
+      sm: '0.125rem',
+      DEFAULT: '0.25rem',
+      DEFAULT: '4px',
+      md: '0.375rem',
+      lg: '0.5rem',
+      full: '99px',
+      large: '12px',
     },
     screens: {
-      sm: { min: "50px", max: "767px" },
+      sm: { min: '50px', max: '1410px' },
       // => @media (min-width: 640px and max-width: 767px) { ... }
 
-      md: { min: "768px", max: "1618px" },
+      md: { min: '1411px', max: '1618px' },
       // => @media (min-width: 768px and max-width: 1023px) { ... }
 
       // lg: { min: "1024px", max: "1279px" },
@@ -31,17 +31,17 @@ module.exports = {
     extend: {
       colors: {
         // primary and secondary
-        primary: "#FFCA80",
-        secondary: "#A800BA",
+        primary: '#FFCA80',
+        secondary: '#A800BA',
 
         // Neutral
 
-        PaleBlue: "#D0EEFF",
-        GrayishBlue: "#8B9393",
-        DarkBlue: "#1B354B",
-        VeryDarkBlue: " #13121D",
-        Blue: "#2596be",
-        white: "#ffffff",
+        PaleBlue: '#D0EEFF',
+        GrayishBlue: '#8B9393',
+        DarkBlue: '#1B354B',
+        VeryDarkBlue: ' #13121D',
+        Blue: '#2596be',
+        white: '#ffffff',
       },
       fontFamily: {
         Raleway: ['"Raleway"'],
